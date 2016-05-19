6a18615f47fd730dfcd2e4702de7a620fcfbf11a
Nathan Leung <18nleung@gmail.com>
2016-05-19 23:39:10 +0000

    initial

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const stylus = require('gulp-stylus');
+const jade = require('gulp-jade');
+const plumber = require('gulp-plumber');
+const path = require('path');
+
+const paths = {
+  babel: ['./src/js/**/*.js'],
+  stylus: ['./src/styl/**/*.styl'],
+  jade: ['./src/**/*.jade', '!./src/_layout.jade'],
+};
+
+gulp.task('default', ['babel', 'stylus', 'jade']);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.babel, ['babel']);
+  gulp.watch(paths.stylus, ['stylus']);
+  gulp.watch(paths.jade, ['jade']);
+});
+
+gulp.task('babel', () => {
+  return gulp.src(paths.babel)
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('stylus', () => {
+  return gulp.src(paths.stylus)
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('jade', () => {
+  return gulp.src(paths.jade)
+    .pipe(plumber())
+    .pipe(jade({
+      basedir: path.join(__dirname, 'src'),
+    }))
+    .pipe(gulp.dest('./build'));
+});
