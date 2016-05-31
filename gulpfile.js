fa9ff14d36ccb124ed56478133f5a7c23ae00d13
Nathan Leung <18nleung@gmail.com>
2016-05-31 22:58:47 +0000

    add autoprefixer cssnano and uglify and partials folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 const stylus = require('gulp-stylus');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
 const jade = require('gulp-jade');
 const plumber = require('gulp-plumber');
 const path = require('path');
@@ -8,7 +11,7 @@ const path = require('path');
 const paths = {
   babel: ['./src/js/**/*.js'],
   stylus: ['./src/styl/**/*.styl'],
-  jade: ['./src/**/*.jade', '!./src/_layout.jade'],
+  jade: ['./src/**/*.jade', '!./src/_layout.jade', '!./src/_partials/**/*.jade'],
 };
 
 gulp.task('default', ['babel', 'stylus', 'jade']);
@@ -23,6 +26,7 @@ gulp.task('babel', () => {
   return gulp.src(paths.babel)
     .pipe(plumber())
     .pipe(babel())
+    .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
 
@@ -30,6 +34,8 @@ gulp.task('stylus', () => {
   return gulp.src(paths.stylus)
     .pipe(plumber())
     .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
     .pipe(gulp.dest('./build/css'));
 });
 
